d6ebf02d70979d449ec378fe69e8401130b17fc0
Caspar van Vuure <70904913+CSPR07@users.noreply.github.com>
2021-09-09 10:20:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,100 +2,115 @@
 <html>
 <head>
     <style>
-        * {
-            box-sizing: border-box;
-        }
+* {
+  box-sizing: border-box;
+}
 
-        body {
-            font-family: Arial;
-            padding: 10px;
-            background-image: url("img/portal.png");
-            background-repeat: repeat;
-        }
+body {
+  font-family: Arial;
+  padding: 0px;
+  background-color: #bfbfbf;
+  background-repeat: repeat;
+}
 
-        .header {
-            padding: 30px;
-            text-align: center;
-            background: white;
-        }
+.wrap{
+    margin: 0;
+    padding: 0;
+}
 
-        .header h1 {
-            font-size: 50px;
-        }
+.header {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  text-align: center;
+  background: #8c1aff;
+  margin-top, margin-left, margin-right: 0px;
+}
 
-        .topnav {
-            overflow: hidden;
-            background-color: #333;
-        }
+.header h1 {
+  font-size: 50px;
+  margin: 0px;
+  text-align: center;
+}
 
-        .topnav a {
-            float: left;
-            display: block;
-            color: #f2f2f2;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-        }
+.topnav {
+  overflow: hidden;
+  background-color: black;
+  padding: 0;
+  padding-left: 43%;
+}
 
-        .topnav a:hover {
-            background-color: #ddd;
-            color: white;
-        }
+.topnav a {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
 
-        .leftcolumn {
-            float: left;
-            width: 74%;
-        }
+.topnav a:hover {
+  background-color: white;
+  color: black;
+}
 
-        .rightcolumn {
-            float: left;
-            width: 24%;
-            padding-left: 20px;
+.leftcolumn {   
+  float: left;
+  width: 25%;
+  padding-right: 20px;
+}
 
-        }
+.rightcolumn {
+  float: left;
+  width: 75%;
+  
+}
 
-        .fakeimg {
-            background-color: #aaa;
-            width: 100%;
-            padding: 20px;
-        }
+.fakeimg {
+  width: 100%;
+  padding: 20px;
+}
 
-        .card {
-            background-color: white;
-            padding: 20px;
-            margin-top: 20px;
-        }
+.card {
+  background-color: white;
+  padding: 20px;
+  margin-top: 20px;
+}
 
-        .row:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
 
-        .footer {
-            padding: 20px;
-            background: #ddd;
-            margin-top: 20px;
-            padding-top: 10px;
-            background-color: white;
-            text-align: center;
-        }
+.footer {
+  padding: 20px;
+  text-align: fixed;
+  background: #ddd;
+  margin-top: 20px;
+  padding-top: 10px;
+  background-color: white;
+  text-align: center;
+}
 
 
-        @media screen and (max-width: 800px) {
-            .leftcolumn, .rightcolumn {
-                width: 100%;
-                padding: 0;
-            }
-        }
+@media screen and (max-width: 1000px) {
+  .leftcolumn, .rightcolumn {   
+    width: 100%;
+    padding: 0;
+  }
+  .topnav{
+      padding-left: 40%;
+  }
+}
 
-        @media screen and (max-width: 400px) {
-            .topnav a {
-                float: none;
-                width: 100%;
-            }
-        }
-    </style>
+@media screen and (max-width: 400px) {
+  .topnav a {
+    float: none;
+    width: 100%;
+  }
+}
+
+</style>
 </head>
 <body>
 <div class="wrap">
@@ -105,15 +120,82 @@
     </div>
 
     <div class="topnav responsive" id="myTopnav">
-        <a href="index.php" class="active">Home</a>
-        <a href="login.php">Login</a>
-        <a href="logout.php">Logout</a>
-        <a href="register.php">Register</a>
-        </a>
+        <a href="index.php" class="active">Homepage</a>
+        <a href="overmij.php">Over mij</a>
+        <!--<a href="login.php" class="div123">Login</a>
+        <a href="logout.php" class="div123">Logout</a>
+        <a href="register.php" class="div123">Register</a>-->
     </div>
     <div class="row">
 
-        <div class="leftcolumn">
+    <div class="leftcolumn">
+            <div class="films">
+                <div class="card">
+                    <h2>Top 3 Nieuwe films</h2>
+                    <div class="fakeimg">
+                        <?php
+                        $conn = mysqli_connect("localhost", "id17176924_root", "cKWw]H8Nu4+piGwW", "id17176924_fullstackproject");
+                        $sql = "SELECT filmname, datumschema FROM films WHERE id = 1;";
+                        $result = $conn-> query($sql);
+
+                        if ($result -> num_rows > 0){
+                            while($row = $result-> fetch_assoc()){
+                                echo "<h2>".$row["filmname"] ."</h2>
+                                <p>Suicide squad (remake) is een goede nieuwe film die uit gaat komen. De trailer laat ons het oude character harley quin zien die in eerdere s.q. films ook is gekomen</p>
+                                <p>" .$row["datumschema"] ."</p>";
+                            }
+                        }
+                        else{
+                            echo "0 Result";
+                        }
+
+                        $conn -> close();
+                        ?>
+                    </div>
+                    <div class="fakeimg">
+                        <?php
+                        $conn = mysqli_connect("localhost", "id17176924_root", "cKWw]H8Nu4+piGwW", "id17176924_fullstackproject");
+                        $sql = "SELECT filmname, datumschema FROM films WHERE id = 2;";
+                        $result = $conn-> query($sql);
+
+                        if ($result -> num_rows > 0){
+                            while($row = $result-> fetch_assoc()){
+                                echo "<h2>".$row["filmname"] ."</h2>
+                                <p>Een hele nieuwe batman film waar we in hopen weer een nieuwe soort joker te kunnen zien die weer gaat vechten met batman. dit zou weer een beetje nostalgie oproepen uit de oudere batman fans</p>
+                                <p>" .$row["datumschema"] ."</p>";
+                            }
+                        }
+                        else{
+                            echo "0 Result";
+                        }
+
+                        $conn -> close();
+                        ?>
+                    </div>
+                    <div class="fakeimg">
+                        <?php
+                        $conn = mysqli_connect("localhost", "id17176924_root", "cKWw]H8Nu4+piGwW", "id17176924_fullstackproject");
+                        $sql = "SELECT filmname, datumschema FROM films WHERE id = 3;";
+                        $result = $conn-> query($sql);
+
+                        if ($result -> num_rows > 0){
+                            while($row = $result-> fetch_assoc()){
+                                echo "<h2>".$row["filmname"] ."</h2>
+                                <p>De nieuwe uncharted movie waar we in de trailer zien dat de hoofdrollen gespeelt worden door Tom Holland (de huidige spiderman in de marvel films) en Mark Walhberg (het kind in ted2 die de pratende beer heeft</p>
+                                <p>" .$row["datumschema"] ."</p>";
+                            }
+                        }
+                        else{
+                            echo "0 Result";
+                        }
+
+                        $conn -> close();
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rightcolumn">
 
             <div class="card">
                 <?php
@@ -170,72 +252,12 @@
 
                 $conn -> close();
                 ?>
-            </div>
-        </div>
-
-        <div class="rightcolumn">
-            <div class="films">
-                <div class="card">
-                    <h2>Top 3 Nieuwe films</h2>
-                    <div class="fakeimg">
-                        <?php
-                        $conn = mysqli_connect("localhost", "id17176924_root", "cKWw]H8Nu4+piGwW", "id17176924_fullstackproject");
-                        $sql = "SELECT filmname, datumschema FROM films WHERE id = 1;";
-                        $result = $conn-> query($sql);
-
-                        if ($result -> num_rows > 0){
-                            while($row = $result-> fetch_assoc()){
-                                echo "<h2>".$row["filmname"] ."</h2><p>" .$row["datumschema"] ."</p>";
-                            }
-                        }
-                        else{
-                            echo "0 Result";
-                        }
-
-                        $conn -> close();
-                        ?>
-                    </div>
-                    <div class="fakeimg">
-                        <?php
-                        $conn = mysqli_connect("localhost", "id17176924_root", "cKWw]H8Nu4+piGwW", "id17176924_fullstackproject");
-                        $sql = "SELECT filmname, datumschema FROM films WHERE id = 2;";
-                        $result = $conn-> query($sql);
-
-                        if ($result -> num_rows > 0){
-                            while($row = $result-> fetch_assoc()){
-                                echo "<h2>".$row["filmname"] ."</h2><p>" .$row["datumschema"] ."</p>";
-                            }
-                        }
-                        else{
-                            echo "0 Result";
-                        }
-
-                        $conn -> close();
-                        ?>
-                    </div>
-                    <div class="fakeimg">
-                        <?php
-                        $conn = mysqli_connect("localhost", "id17176924_root", "cKWw]H8Nu4+piGwW", "id17176924_fullstackproject");
-                        $sql = "SELECT filmname, datumschema FROM films WHERE id = 3;";
-                        $result = $conn-> query($sql);
-
-                        if ($result -> num_rows > 0){
-                            while($row = $result-> fetch_assoc()){
-                                echo "<h2>".$row["filmname"] ."</h2><p>" .$row["datumschema"] ."</p>";
-                            }
-                        }
-                        else{
-                            echo "0 Result";
-                        }
-
-                        $conn -> close();
-                        ?>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
-    <div class="footer">
+    
+    
+</div>
+<div class="footer">
         <h4>Heb jij nog nieuws dat er niet staat, neem dan contact op met mij!</h4>
         <h5>Contact informatie    <br>--------------------------------------------------------<br>
             Email: ditiseenfakeemail@outluuk.com <br> Telefoon: 0612345678 <br> Fax: nee</h5>
