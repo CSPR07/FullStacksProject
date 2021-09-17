daf36c8737613962f73d5e08f88ff5813a222467
Caspar van Vuure <70904913+CSPR07@users.noreply.github.com>
2021-09-17 08:32:14 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,29 +1,29 @@
 <?php
-
+// Initialize the session
 session_start();
-
-if (!isset($_SESSION["userid"]) || $_SESSION["userid"] !== true){
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
 ?>
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome <?php echo $_SESSION["name"];?></title>
-    <link rel="stylesheet" href=https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Helle, <strong><?php echo $_SESSION["name"]; ?></strong> Welcome to my site.</h1>
-        </div>
-        <p>
-            <a href="logout.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">logout</a>
-        </p>
-    </div>
-</div>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
 </body>
 </html>
