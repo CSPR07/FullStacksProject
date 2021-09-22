d1dfa26224a84e338dae1ab9495427e2ff0c0c80
Caspar van Vuure <70904913+CSPR07@users.noreply.github.com>
2021-09-22 12:08:38 +0200

    Update login.php

--- login.php
+++ login.php
@@ -14,6 +14,9 @@
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
             $_SESSION['username'] = $username;
+            while($row = $result -> fetch_assoc()){
+            $_SESSION['userid'] = $row['id'];
+            }
             // Redirect to user dashboard page
             header("Location: index.php");
         } else {
@@ -27,20 +30,50 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+        #text {display:none;color:RED}
+        
+        #psswrd {
+            margin-bottom:25px;
+        }
+    </style>
     <meta charset="utf-8"/>
     <title>Login</title>
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
+    <p id="text">WARNING! Caps lock is ON.</p>
     <form class="form" method="post" name="login">
         <h1 class="login-title">Login</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
-        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="password" id="myInput" class="login-input" name="password" placeholder="Password"/>
+        <input type="checkbox" id = "psswrd" onclick="myFunctio()">Show Password
         <input type="submit" value="Login" name="submit" class="login-button"/>
         <p class="link"><a href="registration.php">New Registration</a></p>
   </form>
-  <?php
-    }
-    ?>
+  <script>
+var input = document.getElementById("myInput");
+var text = document.getElementById("text");
+input.addEventListener("keyup", function(event) {
+
+if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+});
+
+function myFunctio() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+</script>
+<?php
+}
+?>    
 </body>
 </html>
